06e02a0333dac7157eddab23e806f04b58559228
Jeff <doylejeff98@gmail.com>
2022-12-02 15:08:04 -0500

    commit

--- calc.js
+++ calc.js
@@ -1,1 +1,25 @@
 //comment
+//console.log("hi")
+let operator = '0';
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector('#CR');
+    let equal = document.querySelector(".equal");
+
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+          handleNumber(e.target.textContent)
+    }))
+
+
+
+})
+
+function handleNumber(num){
+console.log(num);
+}
+
+
