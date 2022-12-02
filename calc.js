66980d2ad3c3b465b65513864c4113fee1b34775
Jeff <doylejeff98@gmail.com>
2022-12-02 15:54:12 -0500

    commit

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
 //comment
 //console.log("hi")
-let operator = '0';
-let displayValue='';
+let operator = '';
+let previousValue='';
+let currentValue='';
 
 document.addEventListener("DOMContentLoaded", function(){
     let clear = document.querySelector('#CR');
@@ -11,8 +12,12 @@ document.addEventListener("DOMContentLoaded", function(){
     let numbers = document.querySelectorAll(".number");
     let operators = document.querySelectorAll(".operator");
 
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+
     numbers.forEach((number) => number.addEventListener("click", function(e){
           handleNumber(e.target.textContent)
+          currentScreen.textContent = currentValue;
     }))
 
 
@@ -20,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function handleNumber(num){
-displayValue += num;
+currentValue += num;
 }
 
 
